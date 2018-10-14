f6e445a2f52374ddcd2ff1239b888a09e403a539
sumitramanga <sumitramanga@gmail.com>
2018-10-15 11:41:57 +1300

    Added placeholder text:

--- update.php
+++ update.php
@@ -16,18 +16,19 @@
 			  <form>
 			    <div class="form-group" method="post" enctype="multipart/form-data" >
 			    	<label class="label" for="bookTitle">Book Title</label>
-					<input type="text" class="form-control" id="bookTitle">
+					<input type="text" class="form-control" id="bookTitle" placeholder="The Secret">
 			    </div>
 			    <div class="form-group">
 					<label class="label" for="authorName">Author Name</label>
-   					<input type="text" class="form-control" id="authorName">
+   					<input type="text" class="form-control" id="authorName" placeholder="Rhonda Byrne">
 			    </div>
 			    <div class="form-group">
 					<label for="description" class="label">Description</label>
-					<textarea type="textarea" rows="5" class="form-control" id="description"></textarea>
+					<textarea type="textarea" rows="5" class="form-control" id="description">The Secret is a best-selling 2006 self-help book by Rhonda Byrne, based on the earlier film of the same name. It is based on the law of attraction, which claims that thoughts can change the world directly. The book has sold 30 million copies worldwide and has been translated into 50 languages.</textarea>
 			    </div>
 
 				<div class="form-group">
+					<img src="https://via.placeholder.com/350x150"><br>
 					<label for="imageUpload" class="label">Upload Book Cover</label><br>
 					<input type="file" name="imageUpload" id="imageUpload">
 				</div>
