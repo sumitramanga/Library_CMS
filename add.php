d7ce3347c4554452370ae5c08d0f304fbc8a0f54
sumitramanga <sumitramanga@gmail.com>
2018-10-15 21:05:00 +1300

    Created errors for book title, author and description

--- add.php
+++ add.php
@@ -1,4 +1,6 @@
 <?php
+	// require 'vendor/autoload.php';
+
 	$page = 'Add Book';
 	require('templates/head.php');
 
@@ -9,8 +11,29 @@
 
 		if (!$bookTitle) {
 			array_push($errors, 'Title of the book is required');
-			var_dump('no book title');
+		} elseif (strlen($bookTitle) < 2) {
+			array_push($errors, 'Book title too short. Please enter a longer book title.');
+		} else if (strlen($bookTitle) > 100) {
+			array_push($errors, 'Title is too long. Please enter a shorter title.');
 		}
+
+		if (!$author) {
+			array_push($errors, 'Author of the book is required');
+		} elseif (strlen($author) < 2) {
+			array_push($errors, 'Book title too short. Please enter a longer book title.');
+		} else if (strlen($author) > 100) {
+			array_push($errors, 'Title is too long. Please enter a shorter title.');
+		}
+
+		if (!$description) {
+			array_push($errors, 'A description about the book is required.');
+		} elseif (strlen($author) < 2) {
+			array_push($errors, 'Description too short. Please enter a longer description about the book.');
+		} else if (strlen($author) > 100) {
+			array_push($errors, 'Description is too long. Please enter a shorter description.');
+		}
+
+
 	}
  ?>
 
@@ -23,13 +46,24 @@
 
 		  <div class="container">
 			  <h2 class="title">ADD A BOOK</h2>
-			  <form method="post" action="index.php" enctype="multipart/form-data">
+
+			  <?php if($_POST && !empty($errors)): ?>
+				  <div class="alert alert-danger" role="alert">
+				  	<ul>
+				  		<?php foreach($errors as $singleError): ?>
+							<li><?=$singleError; ?></li>
+						<?php endforeach;?>
+				  	</ul>
+				  </div>
+			  <?php endif; ?>
+
+			  <form method="post" action="add.php" enctype="multipart/form-data">
 			    <div class="form-group">
 			    	<label class="label" for="bookTitle">Book Title</label>
 					<input type="text" name="bookTitle" class="form-control" id="bookTitle">
 			    </div>
 			    <div class="form-group">
-					<label class="label" for="authorName">Author Name</label>
+					<label class="label" for="author">Author Name</label>
    					<input type="text" name="author" class="form-control" id="authorName">
 			    </div>
 			    <div class="form-group">
